52c4a6609cc0c46eee01ebd08f2efdae7235c4e3
Ben <bhturne@g.clemson.edu>
2016-05-23 11:16:36 -0400

    ToDo

---
+++ src/com/theironyard/ToDoItem.java
@@ -0,0 +1,14 @@
+package com.theironyard;
+
+/**
+ * Created by Ben on 5/23/16.
+ */
+public class ToDoItem {
+    String text;
+    Boolean isDone;
+
+    public ToDoItem(String text, boolean isDone){
+        this.text = text;
+        this.isDone = isDone;
+    }
+}
